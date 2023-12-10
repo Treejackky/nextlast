b293ed6699fd103dc02f8290f55d9e691cc88d17
Treejackky <s6304062636324@email.kmutnb.ac.th>
2023-12-10 12:41:46 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,199 @@
-import Image from 'next/image'
+"use client";
+import { on } from "events";
+import Image from "next/image";
+import React from "react";
+import { useEffect, useState } from "react";
+type Props = {};
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+export async function getItem() {
+  const res = await fetch("http://54.179.86.5:8765/v1/item");
+  const data = await res.json();
+  return data;
+}
+
+export default function Home({}: Props) {
+  const isSushi = ["51306", "51307", "51321", "51323","51324",
+, "51325", "51327", "51328", "515020"]
+const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
+  "514002", "514003", "74101", "74108", "74117"]
+  const isSeafood = ["51337", "51338", "51339", "514008", "79152",
+  "74118", "74119", "74129"]
+  const isDesert = ["51347", "51348", "51350", "51351", "5973138"]
+  const isVegeis  =["75101", "75102", "75103", "75104", "75106", "75109", "75110"
+  , "75111", "75125", "79167"]
+  const isNoodle = ["74116", "75115", "75116", "75117"]
+
+  // ลูกค้าเดินไปตักเอง
+  //   const isPizza = ["76102", "76104", "76106", "76109", "76110"]
+  //   const isSnacks = ["76114", "76115", "76117", "76118", "76121",
+  //   "76122", "76123", "76133", "76138", "76143", "77101", "77102",
+  // "77108", "7107", "77109", "77111", "77128", "77133"]
+  // const isDrink = ["78127", "78128", "78129", "79133", "79131", "79130","79129"]
+  const [data, setData] = useState<any>([]);
+  const [dataSushi, setDataSushi] = useState<any>([]);
+  const [dataBeef, setDataBeef] = useState<any>([]);
+  const [dataSeafood, setDataSeafood] = useState<any>([]);
+  const [dataDesert, setDataDesert] = useState<any>([]);
+  const [dataVegeis, setDataVegeis] = useState<any>([]);
+  const [dataNoodle, setDataNoodle] = useState<any>([]);
+
+  useEffect(() => {
+    getItem().then((data) => {
+      setData(data);
+    });
+  }, []);
+  
+  // FILTER DATA
+  useEffect(() => {
+    const sushi = data.filter((item: any) => {
+      return isSushi.includes(item.ItemCode);
+    });
+    const beef = data.filter((item: any) => {
+      return isBeef.includes(item.ItemCode);
+    });
+    const seafood = data.filter((item: any) => {
+      return isSeafood.includes(item.ItemCode);
+    });
+    const desert = data.filter((item: any) => {
+      return isDesert.includes(item.ItemCode);
+    });
+    const vegeis = data.filter((item: any) => {
+      return isVegeis.includes(item.ItemCode);
+    });
+    const noodle = data.filter((item: any) => {
+      return isNoodle.includes(item.ItemCode);
+    });
+    setDataNoodle(noodle);
+    setDataVegeis(vegeis);
+    setDataDesert(desert);
+    setDataSeafood(seafood);
+    setDataBeef(beef);
+    setDataSushi(sushi);
+  }, [data]);
+   
+  const showMap = (data:any) => {
+    return data.map((item: any, index: any) => {
+      return(
+        <div className=""  key={index}> {index}
+          {item.Name} :  {item.UnitPrice == "" ? 0 : item.UnitPrice} บาท
+          {item.ItemCode}
+          <img src={"https://posimg.s3.ap-southeast-1.amazonaws.com/" + item.ItemCode + ".jpg"}  alt="Imgfood" />
         </div>
+      )
+    });
+  }
+ 
+
+  const [page, setPage] = useState<any>(1);
+
+  const Menu = () => {
+    // {"_id":"655fb378b07c32a55a5e0ba3","ItemID":"8869","Disc":"N","GrpMaster":"7","GrpSub":"704","ItemCode":"70406","Name":"C เนื้อริบอาย (กก)","SvcExcl":"X","TaxExcl":"Y","UnitPrice":""}
+  
+    return (
+      <>
+        <div className="Menu">
+        <div>Sushi</div>
+        {showMap(dataSushi)}
+        <div>Beef</div>
+        {showMap(dataBeef)}
+        <div>Seafood</div>
+        {showMap(dataSeafood)}
+        <div>Vegeis</div>
+        {showMap(dataVegeis)}
+        <div>Noodle</div>
+        {showMap(dataNoodle)}
+        <div>Desert</div>
+        {showMap(dataDesert)}
+        </div>
+      </>
+    );
+  }
+
+  const Cart = () => {
+    return (
+      <div>
+        <div>Cart</div>
+        <div>Cart</div>
+        <div>Cart</div>
+        <div>Cart</div>
+        <div>Cart</div>
       </div>
+    );
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+  const Order = () => {
+    return (
+      <div>
+        <div>Order</div>
+        <div>Order</div>
+        <div>Order</div>
+        <div>Order</div>
+        <div>Order</div>
       </div>
+    );
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  const isPage = () => {
+    {if(page == 1){
+      return <Menu />
+    }
+    else if(page == 2){
+      return <Cart />
+    }
+    else if(page == 3){
+      return <Order />
+    }
+    }
+  }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  const isChanged = (e: any) => {
+    console.log(e.target.id);
+    if(e.target.id == "menu"){
+      return setPage(1);
+    }
+    else if(e.target.id == "cart"){
+      return setPage(2);
+    }
+    else if(e.target.id == "order"){
+      return setPage(3);
+    }
+  }
+
+  const isFooter = () => {
+    return(
+        <footer>
+      <button id="menu" onClick={isChanged}>Menu</button>
+      <button id="cart" onClick={isChanged}>Cart</button>
+      <button id="order" onClick={isChanged}>Order</button>
+          </footer>
+    )
+  }
+
+  // < Navbar />
+  const isNavbar = () => {
+    return(
+      <>
+        <img src="https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/285740555_3164288673886606_4866148165288431653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEaTfrd9vfKuS-Ax69h84RpEJ_GGlsl-CEQn8YaWyX4IWBOWot2UpYR-3xZSyyPNmi5NxDLznwh2EQR4ckfGURm&_nc_ohc=ditpKHDmPFoAX-aRrc6&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfCyL4bmRh3lvI7QxeRXeIrIItyhGlL2o_Bi-XVOYZNTwQ&oe=657A440D" alt="logo" />
+       <div className="bar-scroll">
+       <div>Sushi</div>
+        <div>Beef</div>
+        <div >Seafood</div>
+        <div>Vegeis</div>
+        <div>Noodle</div>
+        <div>Desert</div>
+       </div>
+      </>
+    )
+  }
+
+
+  return (
+    <>
+      {isNavbar()}
+      {isPage()}
+      {isFooter()}    
+    </>
+  );
 }
