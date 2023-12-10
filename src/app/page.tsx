a7526b33d69d3f4c459c314712578a408258ae58
Treejackky <s6304062636324@email.kmutnb.ac.th>
2023-12-10 20:13:54 +0700

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { on } from "events";
 import Image from "next/image";
-import React from "react";
+import React, { use } from "react";
 import { useEffect, useState } from "react";
 type Props = {};
 
@@ -10,6 +10,36 @@ export async function getItem() {
   const data = await res.json();
   return data;
 }
+
+export async function postItem(cart:any) {
+
+  let zlib = require('zlib');
+   let body =  zlib.deflateSync(JSON.stringify(cart));
+  let res = await fetch("http://54.179.86.5:8765/v1/order_qr",
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/octet-stream',
+      },
+      body: body.slice(2),
+    }
+     );
+    
+    let resBuffer = await res.arrayBuffer();
+
+    let data_cart =    JSON.parse(zlib.unzipSync(Buffer.from([120, 156, ...new Uint8Array(resBuffer)])));
+
+    return {
+      props: {data_cart},
+    };
+  }
+
+  export async function getOrder() {
+    const res = await fetch("http://54.179.86.5:8765/v1/order");
+    const data_ord = await res.json();
+    console.log(data_ord);
+    return  data_ord;
+  }
 
 export default function Home({}: Props) {
   const isSushi = ["51306", "51307", "51321", "51323","51324",
@@ -30,18 +60,35 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
   // "77108", "7107", "77109", "77111", "77128", "77133"]
   // const isDrink = ["78127", "78128", "78129", "79133", "79131", "79130","79129"]
   const [data, setData] = useState<any>([]);
+  const [order, setOrder] = useState<any>([]);
   const [dataSushi, setDataSushi] = useState<any>([]);
   const [dataBeef, setDataBeef] = useState<any>([]);
   const [dataSeafood, setDataSeafood] = useState<any>([]);
   const [dataDesert, setDataDesert] = useState<any>([]);
   const [dataVegeis, setDataVegeis] = useState<any>([]);
   const [dataNoodle, setDataNoodle] = useState<any>([]);
+  const [dataFilter, setDataFilter] = useState<any>([]);
 
   useEffect(() => {
     getItem().then((data) => {
       setData(data);
     });
   }, []);
+
+  useEffect(() => {
+    getOrder().then((orderData) => {
+      setOrder(orderData);
+    }).catch(error => {
+      console.error('Error fetching order data:', error);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   getOrder().then((order) => {
+  //     setOrder(order);
+  //   });
+  // }, []);
+  
   
   // FILTER DATA
   useEffect(() => {
@@ -69,33 +116,76 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
     setDataSeafood(seafood);
     setDataBeef(beef);
     setDataSushi(sushi);
+    setDataFilter([...dataSushi, ...dataBeef, ...dataSeafood,... dataDesert, ...dataVegeis, ...dataNoodle]);
   }, [data]);
 
-  // local storage to add food 
+
+
   const [cart, setCart] = useState<any>([]);
-  
-  const addToCart = (item: any) => {
-    setCart([...cart, item]);
-  }
+
+  useEffect(() => {
    
-  const showMap = (data:any) => {
-    return data.map((item: any, index: any) => {
-      return(
-        <button className="item"  
-        key={index}
-        onClick={() => addToCart(item)}
-        > {index}
-          {item.Name} :  {item.UnitPrice == "" ? 0 : item.UnitPrice} บาท
-          {/* {item.ItemCode} */}
-          <img src={"https://posimg.s3.ap-southeast-1.amazonaws.com/" + item.ItemCode + ".jpg"}  alt="Imgfood" />
-        </button>
-      )
+    let cart_local = localStorage.getItem("cart");
+     if(cart_local){
+      setCart(JSON.parse(cart_local!));
+      console.log(cart_local);   
+    }
+  
+  },[]);
+
+  const addToCart = (item :any, countItem:any) => {
+  setCart((prevCart: any[])  => {
+     const existingItemIndex = prevCart.findIndex(cartItem => cartItem.ItemCode === item.ItemCode);
+
+    if (existingItemIndex >= 0) {
+       const updatedCart = [...prevCart];
+      updatedCart[existingItemIndex] = {
+        ...updatedCart[existingItemIndex],
+        Quantity: updatedCart[existingItemIndex].Quantity + countItem
+      };
+      return updatedCart;
+    } else {
+       let newItem = {
+        'OutletID': '1.10',
+        'TableID': '14',
+        'ItemID': item.ItemID,
+        'ItemCode': item.ItemCode,
+        'ItemSupp': item.Name,
+        'UnitPrice': item.UnitPrice,
+        'Disc': item.Disc,
+        'Size': '1',
+        'SvcExcl': item.SvcExcl,
+        'TaxExcl': item.TaxExcl,
+        'GrpMaster': item.GrpMaster,
+        'GrpSub': item.GrpSub,
+        'Quantity': countItem,
+        'CashierID': '1',
+      };
+      return [...prevCart, newItem];
+    }
     });
-  }
- 
+  };
+  
+
+  useEffect(() => {
+    if(cart.length > 0){
+      localStorage.setItem("cart", JSON.stringify(cart!));
+    }
+  }, [cart]);
+  
 
   const [page, setPage] = useState<any>(1);
+  const [overlay, setOverlay] = useState<any>(false);
+  const [selectedItem, setSelectedItem] = useState<any>(null);
+  const [countItem, setCountItem] = useState(1);
 
+
+
+  const handleItemClick = (item : any) => {
+    setSelectedItem(item);
+    setOverlay(true);
+  };
+  
   const Menu = () => {
     // {"_id":"655fb378b07c32a55a5e0ba3","ItemID":"8869","Disc":"N","GrpMaster":"7","GrpSub":"704","ItemCode":"70406","Name":"C เนื้อริบอาย (กก)","SvcExcl":"X","TaxExcl":"Y","UnitPrice":""}
   
@@ -109,50 +199,128 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
         <div>เส้น</div>
         <div>ของหวาน</div>
        </nav>
-      <section>
-        <div>Sushi</div>
-        {showMap(dataSushi)}
-        <div>Beef</div>
-        {showMap(dataBeef)}
-        <div>Seafood</div>
-        {showMap(dataSeafood)}
-        <div>Vegeis</div>
-        {showMap(dataVegeis)}
-        <div>Noodle</div>
-        {showMap(dataNoodle)}
-        <div>Desert</div>
-        {showMap(dataDesert)}
+       <section>
+        {dataFilter.map((item:any, index :any) => (
+          <button 
+            className="item" 
+            key={index} 
+            onClick={() => handleItemClick(item)}
+          >
+            {item.Name}
+            <img src={`https://posimg.s3.ap-southeast-1.amazonaws.com/${item.ItemCode}.jpg`} alt="Imgfood" />
+          </button>
+        ))}
+       {overlay && selectedItem && (
+          <div className="modal">
+            <div className="modal-header">
+              <div className="name">{selectedItem.Name}</div>
+            </div>
+            <div className="modal-body">
+              <div className="modal-img">
+                <img src={`https://posimg.s3.ap-southeast-1.amazonaws.com/${selectedItem.ItemCode}.jpg`}  alt="Imgfood" />
+              </div>
+              <div className="item-count">
+              <button className="count-btn" onClick={() => setCountItem(prevCount => prevCount >1 ? prevCount - 1 : 1)}>-</button>
+                <span>{countItem}</span>
+                <button className="count-btn" onClick={() => setCountItem(prevCount => prevCount + 1)}>+</button>
+
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="modal-action" onClick={() => [setOverlay(false), setCountItem(1)]}>ยกเลิก</button>
+              <button className="modal-action" onClick={() => [ setOverlay(false), addToCart(selectedItem, countItem), setCountItem(1)]}>เพิ่มในตะกร้า</button>
+            </div>
+          </div>
+        )}
+
       </section>
       </>
     );
   }
 
   const Cart = () => {
+    const incrementQuantity = (itemCode:any) => {
+      setCart(cart.map((item: { ItemCode: any; Quantity: number; }) => 
+        item.ItemCode === itemCode ? { ...item, Quantity: item.Quantity + 1 } : item
+      ));
+    };
+  
+    const decrementQuantity = (itemCode: any) => {
+      setCart(cart.map((item: { ItemCode: any; Quantity: number; }) =>
+        item.ItemCode === itemCode && item.Quantity > 1 ? { ...item, Quantity: item.Quantity - 1 } : item
+      ));
+    };
+  
     return (
       <>
-        <div>Cart</div>
-        <div>Cart</div>
-        <div>Cart</div>
-        <div>Cart</div>
-        <div>Cart</div>
-      </>
+  <div className="cart-container">
+    {cart.map((item:any, index:any) => (
+      <div className="cart-item" key={index}>
+       <div className="cart-header">
+       {item.ItemSupp}
+        <img src={`https://posimg.s3.ap-southeast-1.amazonaws.com/${item.ItemCode}.jpg`} alt="Imgfood" />
+        </div>
+        <div className="cart-body">
+          <div className="quantity-controls">
+            <button onClick={() => decrementQuantity(item.ItemCode)} aria-label="Decrease quantity">-</button>
+            <span>{item.Quantity}</span>
+            <button onClick={() => incrementQuantity(item.ItemCode)} aria-label="Increase quantity">+</button>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+  <div className="parent-container">
+    <div className="cart-order">
+      <button onClick={() =>[
+        postItem(cart),
+        setCart([]),
+        localStorage.removeItem("cart"),
+      ]}>สั่ง {cart.length} รายการ</button>
+    </div>
+  </div>
+</>
+
     );
-  }
+  };
+  
 
   const Order = () => {
+  
     return (
       <>
-        <div>Order</div>
-        <div>Order</div>
-        <div>Order</div>
-        <div>Order</div>
-        <div>Order</div>
+         {order.map((item:any, index:any) => (
+        <div className="order-item" key={index}>
+          <div className="order-header">
+          <img src={`https://posimg.s3.ap-southeast-1.amazonaws.com/${item.ItemCode}.jpg`} alt="Imgfood" />
+
+            <div className="order-id">{item.ItemSupp}</div>
+          </div>
+          <div className="order-body">
+             <div className="order-total">จำนวนทั้งหมด: {item.Quantity}</div>
+          </div>
+        </div>
+         ))}
       </>
     );
   }
 
-
-
+  // {
+  //   'OutletID': '1.10',
+  //   'TableID': '14',
+  //   'ItemID': data2.ItemID,
+  //   'ItemCode': data2.ItemCode,
+  //   'ItemSupp': data2.Name,
+  //   'UnitPrice': data2.UnitPrice,
+  //   'Disc': data2.Disc,
+  //   'Size': '1',
+  //   'SvcExcl': data2.SvcExcl,
+  //   'TaxExcl': data2.TaxExcl,
+  //   'GrpMaster': data2.GrpMaster,
+  //   'GrpSub': data2.GrpSub,
+  //   'Quantity': quanity,
+  //   'CashierID': '1',
+  // },
   const isPage = () => {
     {if(page == 1){
       return <Menu />
@@ -161,6 +329,7 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
       return <Cart />
     }
     else if(page == 3){
+     
       return <Order />
     }
     }
@@ -175,37 +344,43 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
       return setPage(2);
     }
     else if(e.target.id == "order"){
+      getOrder().then((orderData) => {
+        setOrder(orderData);
+      }).catch(error => {
+        console.error('Error fetching order data:', error);
+      });
       return setPage(3);
     }
   }
 
   // < Navbar />
   const isHeader = () => {
-    return(
-      <>
-        <img src="https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/285740555_3164288673886606_4866148165288431653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEaTfrd9vfKuS-Ax69h84RpEJ_GGlsl-CEQn8YaWyX4IWBOWot2UpYR-3xZSyyPNmi5NxDLznwh2EQR4ckfGURm&_nc_ohc=ditpKHDmPFoAX-aRrc6&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfCyL4bmRh3lvI7QxeRXeIrIItyhGlL2o_Bi-XVOYZNTwQ&oe=657A440D" alt="logo" />
-      </>
-    )
+    if(page == 1){
+      return(
+        <>
+          <img src="https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/285740555_3164288673886606_4866148165288431653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEaTfrd9vfKuS-Ax69h84RpEJ_GGlsl-CEQn8YaWyX4IWBOWot2UpYR-3xZSyyPNmi5NxDLznwh2EQR4ckfGURm&_nc_ohc=ditpKHDmPFoAX-aRrc6&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfCyL4bmRh3lvI7QxeRXeIrIItyhGlL2o_Bi-XVOYZNTwQ&oe=657A440D" alt="logo" />
+        </>
+      )
+    }else if(page == 2){
+      return <></>
+    }
   }
 
   const isFooter = () => {
     return(
-        <>
+      <>
       <button id="menu" onClick={isChanged}>Menu</button>
       <button id="cart" onClick={isChanged}>Cart</button>
       <button id="order" onClick={isChanged}>Order</button>
-          </>
+      </>
     )
   }
 
-
-
-
   return (
     <>
-    <header>
-    {isHeader()}
-    </header>
+     <header className={page === 1 ? "p1" : "p2"}>
+     {isHeader()}
+     </header>
      <div className="main-content">
      {isPage()}
      </div>
