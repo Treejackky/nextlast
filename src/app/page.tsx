7e9ebf81834fcff5a07acfdc073789a540ffbd45
mebiol <s6304062636324@email.kmutnb.ac.th>
2023-12-10 14:51:12 +0700

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,15 +70,25 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
     setDataBeef(beef);
     setDataSushi(sushi);
   }, [data]);
+
+  // local storage to add food 
+  const [cart, setCart] = useState<any>([]);
+  
+  const addToCart = (item: any) => {
+    setCart([...cart, item]);
+  }
    
   const showMap = (data:any) => {
     return data.map((item: any, index: any) => {
       return(
-        <div className=""  key={index}> {index}
+        <button className="item"  
+        key={index}
+        onClick={() => addToCart(item)}
+        > {index}
           {item.Name} :  {item.UnitPrice == "" ? 0 : item.UnitPrice} บาท
-          {item.ItemCode}
+          {/* {item.ItemCode} */}
           <img src={"https://posimg.s3.ap-southeast-1.amazonaws.com/" + item.ItemCode + ".jpg"}  alt="Imgfood" />
-        </div>
+        </button>
       )
     });
   }
@@ -91,7 +101,15 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
   
     return (
       <>
-        <div className="Menu">
+      <nav>
+        <div>ซูชิ</div>
+        <div>เนื้อสัตว์</div>
+        <div >ทะเล</div>
+        <div>ผัก</div>
+        <div>เส้น</div>
+        <div>ของหวาน</div>
+       </nav>
+      <section>
         <div>Sushi</div>
         {showMap(dataSushi)}
         <div>Beef</div>
@@ -104,32 +122,32 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
         {showMap(dataNoodle)}
         <div>Desert</div>
         {showMap(dataDesert)}
-        </div>
+      </section>
       </>
     );
   }
 
   const Cart = () => {
     return (
-      <div>
+      <>
         <div>Cart</div>
         <div>Cart</div>
         <div>Cart</div>
         <div>Cart</div>
         <div>Cart</div>
-      </div>
+      </>
     );
   }
 
   const Order = () => {
     return (
-      <div>
+      <>
         <div>Order</div>
         <div>Order</div>
         <div>Order</div>
         <div>Order</div>
         <div>Order</div>
-      </div>
+      </>
     );
   }
 
@@ -161,39 +179,39 @@ const isBeef = ["51317", "51318", "51320", "74134", "51353", "514005" ,"514007",
     }
   }
 
-  const isFooter = () => {
-    return(
-        <footer>
-      <button id="menu" onClick={isChanged}>Menu</button>
-      <button id="cart" onClick={isChanged}>Cart</button>
-      <button id="order" onClick={isChanged}>Order</button>
-          </footer>
-    )
-  }
-
   // < Navbar />
-  const isNavbar = () => {
+  const isHeader = () => {
     return(
       <>
         <img src="https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/285740555_3164288673886606_4866148165288431653_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEaTfrd9vfKuS-Ax69h84RpEJ_GGlsl-CEQn8YaWyX4IWBOWot2UpYR-3xZSyyPNmi5NxDLznwh2EQR4ckfGURm&_nc_ohc=ditpKHDmPFoAX-aRrc6&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfCyL4bmRh3lvI7QxeRXeIrIItyhGlL2o_Bi-XVOYZNTwQ&oe=657A440D" alt="logo" />
-       <div className="bar-scroll">
-       <div>Sushi</div>
-        <div>Beef</div>
-        <div >Seafood</div>
-        <div>Vegeis</div>
-        <div>Noodle</div>
-        <div>Desert</div>
-       </div>
       </>
     )
   }
 
+  const isFooter = () => {
+    return(
+        <>
+      <button id="menu" onClick={isChanged}>Menu</button>
+      <button id="cart" onClick={isChanged}>Cart</button>
+      <button id="order" onClick={isChanged}>Order</button>
+          </>
+    )
+  }
+
+
+
 
   return (
     <>
-      {isNavbar()}
-      {isPage()}
-      {isFooter()}    
+    <header>
+    {isHeader()}
+    </header>
+     <div className="main-content">
+     {isPage()}
+     </div>
+     <footer>
+     {isFooter()}    
+     </footer>
     </>
   );
 }
